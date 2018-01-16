8d32510d7b3392039a73563907b6a0bb35932c7c
Greg Stimson <stimsong@yahoo.com>
2018-01-16 18:33:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist = { artistName: "The Corrs", songTitle: "Runaway" };
 function updatePlaylist(playlist, artistName, songTitle) {
 //  return Object.assign({}, playlst, { [artistName]: songTitle });
   playlist[artistName] = songTitle;
-  return;
+  return playlist;
   
 }
