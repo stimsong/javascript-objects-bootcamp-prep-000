351f1cd0197361e603936ee437f6e2734662816d
Greg Stimson <stimsong@yahoo.com>
2018-01-16 18:38:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { artistName: "Kanye", songTitle: "" };
+var playlist = { artistName: "Kanye", songTitle: "Gold Digger" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
