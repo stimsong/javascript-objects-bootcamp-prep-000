963a02256b08c6ab67c1159faf4d58691e5699a1
Greg Stimson <stimsong@yahoo.com>
2018-01-16 19:00:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  console.log(delete playlist.artistName);
+  delete playlist.artistName;
   return playlist;
 }
